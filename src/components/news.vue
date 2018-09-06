fd44101e6fa0e185739814badb6fbe812c9f9360
Light-et <light.elitetech@gmail.com>
2018-09-06 18:28:40 +0700

    copy& clear code

---
+++ src/components/news.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-carousel class="component-top" style="max-width: 700px;margin:auto; height:300px">
+    <q-carousel-slide :img-src="item.img">
+      <div class="absolute-top-left custom-caption-top">
+        <div class="subtitle text-white q-ml-sm q-mt-sm" >{{item.sub}}</div>
+      <div class="q-display-1 text-white q-ml-sm q-mb-sm" >{{item.title}}</div>
+        <q-btn class=" fixed-bottom-left q-ml-sm q-mb-sm" :class="{ hidden: item.isBtnHidden }" color="white" text-color="brown"  rounded>
+          <q-item  @click.native="show">
+            <q-item-main :label="item.label" />
+          </q-item>
+        </q-btn>
+        <slot></slot>
+      </div>
+    </q-carousel-slide>
+  </q-carousel>
+</template>
+<script>
+export default {
+  props: {
+    item: [Object],
+  },
+  methods: {
+    show() {
+      this.$slots.default[0].componentInstance.opened = true
+    },
+  },
+}
+</script>
+<style>
+</style>
