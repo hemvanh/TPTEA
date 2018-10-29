cb2023c1c407ae0613111f7a72fd844b56310334
Light-et <light.elitetech@gmail.com>
2018-10-29 19:13:52 +0700

    Draw QRcode

---
+++ src/components/QRcode.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <canvas id="canvas"></canvas>
+</div>
+</template>
+<script>
+import QRCode from 'qrcode'
+export default {
+  props: {
+    qrcode: [String, Object],
+  },
+  mounted() {
+    var canvas = this.$el.querySelector('#canvas')
+    QRCode.toCanvas(canvas, this.qrcode, function(error) {
+      if (error) console.error(error)
+      console.log('success!')
+    })
+  },
+}
+</script>
+<style>
