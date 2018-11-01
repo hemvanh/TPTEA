d1d0f19871ecc68cdc14e5293f8244a86a160675
Light-et <light.elitetech@gmail.com>
2018-11-01 15:12:58 +0700

    push again file

---
+++ src/components/GenQRCode.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <canvas id="canvas"></canvas>
+</div>
+</template>
+<script>
+import {_procError} from '../util/common'
+import QRCode from 'qrcode'
+export default {
+  props: {
+    qrcode: {
+      type: String,
+      default: () => 'xxx',
+    },
+    dark: {
+      type: String,
+      default: () => '#00F',
+    },
+    light: {
+      type: String,
+      default: () => '#0000',
+    },
+    version: {
+      type: Number,
+      default: () => 5,
+    },
+  },
+  mounted() {
+    var canvas = this.$el.querySelector('#canvas')
+    QRCode.toCanvas(canvas, this.qrcode, {
+      color: {
+        dark: this.dark,
+        light: this.light,
+      },
+      version: this.version,
+    }).catch(err => {
+      _procError(err)
+    })
+  },
+}
+</script>
+<style>
