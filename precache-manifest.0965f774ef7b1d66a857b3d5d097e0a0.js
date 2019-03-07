44a4fac59ef58f127772260d0c180e275f3f543b
John-et <tripham178@gmail.com>
2019-03-07 13:31:07 +0700

    testing

--- precache-manifest.0965f774ef7b1d66a857b3d5d097e0a0.js
+++ precache-manifest.0965f774ef7b1d66a857b3d5d097e0a0.js
@@ -28,52 +28,56 @@ self.__precacheManifest = [
     "url": "statics/splash/ipadpro2_splash.png"
   },
   {
-    "revision": "b04bb7c35df8428346644bdd1a768a21",
-    "url": "statics/main-category-hot.jpg"
-  },
-  {
-    "revision": "34e14a8932d3d3f4cc568db480b7e503",
-    "url": "statics/category-05.jpg"
-  },
-  {
     "revision": "5ca0bb2397976925b63560b620c8fddd",
     "url": "statics/splash/ipadpro3_splash.png"
   },
   {
-    "revision": "e0e41797ce02ad7afa59302519f65062",
-    "url": "statics/splash/iphonexsmax_splash.png"
+    "revision": "b04bb7c35df8428346644bdd1a768a21",
+    "url": "statics/main-category-hot.jpg"
   },
   {
     "revision": "e823136824cab8b5cad0a52aee5596ef",
     "url": "statics/splash/ipadpro1_splash.png"
   },
   {
-    "revision": "943b239ce0d615228200f7190de047d8",
-    "url": "statics/icons/favicon-32x32.png"
+    "revision": "4f11d96ddf31938e9cdb732d7c15e151",
+    "url": "statics/splash/ipad_splash.png"
+  },
+  {
+    "revision": "cdec18eac0fb4339eba6bdeff36f75c4",
+    "url": "statics/splash/iphonex_splash.png"
+  },
+  {
+    "revision": "e7d738849c406fb0708b6e212dabf9a2",
+    "url": "statics/icons/favicon-16x16.png"
   },
   {
     "revision": "94927b05c26b2a1b4d585c7305b9d04d",
     "url": "statics/icons/apple-icon-152x152.png"
   },
   {
-    "revision": "c285996d6f7dbef24ebbbfbe630e812a",
-    "url": "statics/icons/china-flag.png"
+    "revision": "943b239ce0d615228200f7190de047d8",
+    "url": "statics/icons/favicon-32x32.png"
   },
   {
-    "revision": "77bce50393376332bc7b948a33f1143f",
-    "url": "statics/icons/icon-192x192.png"
+    "revision": "c285996d6f7dbef24ebbbfbe630e812a",
+    "url": "statics/icons/china-flag.png"
   },
   {
     "revision": "7d658a65f98809eb34686beb06e127d3",
     "url": "statics/icons/icon-128x128.png"
   },
   {
-    "revision": "262f954ddadfae773a3548b2330ea819",
-    "url": "statics/icons/taiwan-flag.png"
+    "revision": "77bce50393376332bc7b948a33f1143f",
+    "url": "statics/icons/icon-192x192.png"
   },
   {
     "revision": "c3033218b49491bb0ac4cc87d10c8ed8",
     "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "262f954ddadfae773a3548b2330ea819",
+    "url": "statics/icons/taiwan-flag.png"
   },
   {
     "revision": "6af85f4c91ff8304e9448667906a39c4",
@@ -100,36 +104,28 @@ self.__precacheManifest = [
     "url": "statics/icons/icon-384x384.png"
   },
   {
-    "revision": "28f75e604a5048ee11cf3665ce1ba471",
-    "url": "statics/icons/icon-256x256.png"
-  },
-  {
     "revision": "c2b35452f36dcfc83a03c07a8ead6104",
     "url": "statics/icons/icon-512x512.png"
   },
   {
-    "revision": "c85d03b60f2670a5b74b42b8be8533ea",
-    "url": "statics/menu-05.jpg"
-  },
-  {
-    "revision": "9f528180cb170f84786f1ea2b02f4b9b",
-    "url": "statics/menu-04.jpg"
-  },
-  {
-    "revision": "e7d738849c406fb0708b6e212dabf9a2",
-    "url": "statics/icons/favicon-16x16.png"
+    "revision": "28f75e604a5048ee11cf3665ce1ba471",
+    "url": "statics/icons/icon-256x256.png"
   },
   {
     "revision": "51f995bae36abb16ade8f9684be259f3",
     "url": "statics/menu-03.jpg"
   },
   {
-    "revision": "363ac526ab040cc6a287",
-    "url": "js/app.58e5032.js"
+    "revision": "9f528180cb170f84786f1ea2b02f4b9b",
+    "url": "statics/menu-04.jpg"
   },
   {
-    "revision": "a54d4cff09320803d08dc3c14eb436ad",
-    "url": "statics/category-01.jpg"
+    "revision": "c85d03b60f2670a5b74b42b8be8533ea",
+    "url": "statics/menu-05.jpg"
+  },
+  {
+    "revision": "363ac526ab040cc6a287",
+    "url": "js/app.58e5032.js"
   },
   {
     "revision": "921acf05c64b35bee26e6ea08de312b1",
@@ -138,6 +134,10 @@ self.__precacheManifest = [
   {
     "revision": "1fa7aa13b79ee66027c54d2df27e0f56",
     "url": "statics/menu-11.jpg"
+  },
+  {
+    "revision": "a54d4cff09320803d08dc3c14eb436ad",
+    "url": "statics/category-01.jpg"
   },
   {
     "revision": "c38f90d42db60f76c44a13d9ca345b04",
@@ -152,16 +152,12 @@ self.__precacheManifest = [
     "url": "statics/category-08.jpg"
   },
   {
-    "revision": "79699e94132fb0cab6a25a43e7124ad5",
-    "url": "statics/menu-02.jpg"
-  },
-  {
     "revision": "9dbee0a57949675c629738a3ed4f9f8f",
     "url": "statics/menu-01.jpg"
   },
   {
-    "revision": "dc6ae0779b40cefaa09d95e6a9dfd3ba",
-    "url": "statics/quasar-logo.png"
+    "revision": "79699e94132fb0cab6a25a43e7124ad5",
+    "url": "statics/menu-02.jpg"
   },
   {
     "revision": "17e3b1c631f1e2e61f1d29f6a6500c47",
@@ -172,6 +168,10 @@ self.__precacheManifest = [
     "url": "statics/menu-07.jpg"
   },
   {
+    "revision": "dc6ae0779b40cefaa09d95e6a9dfd3ba",
+    "url": "statics/quasar-logo.png"
+  },
+  {
     "revision": "9f9f4ad95f1d8e87a648f2373a657ec4",
     "url": "statics/splash/iphone5_splash.png"
   },
@@ -180,12 +180,12 @@ self.__precacheManifest = [
     "url": "statics/category-04.jpg"
   },
   {
-    "revision": "94ba98e9f9ccb0d51f88f3e0037bce1e",
-    "url": "statics/news-bottom.jpg"
-  },
-  {
     "revision": "9bbdaeda8887f8613d1c915388c65a28",
     "url": "statics/splash/iphone6_splash.png"
+  },
+  {
+    "revision": "94ba98e9f9ccb0d51f88f3e0037bce1e",
+    "url": "statics/news-bottom.jpg"
   },
   {
     "revision": "b1a1f6d94a9e447a0ac5d60aa8372295",
@@ -200,16 +200,16 @@ self.__precacheManifest = [
     "url": "statics/splash/iphoneplus_splash.png"
   },
   {
-    "revision": "4f11d96ddf31938e9cdb732d7c15e151",
-    "url": "statics/splash/ipad_splash.png"
-  },
-  {
-    "revision": "cdec18eac0fb4339eba6bdeff36f75c4",
-    "url": "statics/splash/iphonex_splash.png"
+    "revision": "34e14a8932d3d3f4cc568db480b7e503",
+    "url": "statics/category-05.jpg"
   },
   {
     "revision": "791d460848051485414e037bfbcf3452",
     "url": "statics/news1.jpg"
+  },
+  {
+    "revision": "e0e41797ce02ad7afa59302519f65062",
+    "url": "statics/splash/iphonexsmax_splash.png"
   },
   {
     "revision": "d9c608dcea837a8f2c275b0b874da757",
