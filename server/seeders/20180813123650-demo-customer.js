d4675dd8d97d9bdf4a5ad2cfcbe62627b44f194f
phong-et <phillip7.et@gmail.com>
2023-07-28 07:49:13 +0700

    revive front-end, db migrate and some seeder files

--- server/seeders/20180813123650-demo-customer.js
+++ server/seeders/20180813123650-demo-customer.js
@@ -1,29 +1,21 @@
-'use strict'
-
-var GoogleSpreadsheet = require('google-spreadsheet')
-var _d = require('lodash')
-var creds = require('../TP-TEA-HK-4be78b7ad5f8.json')
-var doc = new GoogleSpreadsheet('1qFpMLDWABXh2JFg_h6VZbZt5OxslSuvjzS1Zn74uSag')
-function getData() {
-  return new Promise((resolve, reject) => {
-    doc.useServiceAccountAuth(creds, function(err) {
-      doc.getRows(1, function(err, rows) {
-        if (err) reject(err)
-        else resolve(rows)
-      })
-    })
-  })
-}
+var {faker} = require('@faker-js/faker')
+const md5 = require('md5')
 module.exports = {
   async up(queryInterface, Sequelize) {
-    var data = await getData().catch(err => console.log(err))
-    return queryInterface.bulkInsert(
-      'customers',
-      _d.map(data, row =>
-        _d.pick(row, 'id', 'name', 'address', 'phone', 'username', 'password', 'points', 'balance', 'type')
-      ),
-      {}
-    )
+    var customers = []
+    Array.from({length: 1000}).forEach(() => {
+      customers.push({
+        name: faker.person.fullName(),
+        address: faker.location.city(),
+        phone: faker.phone.number(),
+        username: faker.internet.userName(),
+        password: md5(123),
+        points: faker.number.int({min: 1, max: 1000}),
+        balance: faker.number.int({min: 500, max: 100000}),
+      })
+    })
+    console.log(customers)
+    return queryInterface.bulkInsert('customers', customers, {})
   },
 
   down: (queryInterface, Sequelize) => {
